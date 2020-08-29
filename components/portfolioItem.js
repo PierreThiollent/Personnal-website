8e21fa0763869507ca8c8c993e60e1f053c35044
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-08-30 01:21:25 +0200

    📦 FEAT: Add content into PortfolioItem component

--- components/portfolioItem.js
+++ components/portfolioItem.js
@@ -1,8 +1,18 @@
-import { Flex } from '@chakra-ui/core';
+import { Box, Flex, Image, Link, Text } from '@chakra-ui/core';
 
-const PortfolioItem = () => (
-  <Flex flex='0 1 350px' px='15px'>
-    Hello
+const PortfolioItem = ({ image, projectName, projectDescription, link }) => (
+  <Flex flex='0 1 370px' mb='30px'>
+    <Link _hover={{ textDecoration: 'none' }} href={`${link}`} isExternal>
+      <Image src={`/${image}`} borderRadius='5px' maxHeight='370px' />
+      <Box mt='20px'>
+        <Text fontSize='18px' color='darkblue'>
+          {projectName}
+        </Text>
+        <Text color='customGrey' fontWeight='300' fontSize='14px'>
+          {projectDescription}
+        </Text>
+      </Box>
+    </Link>
   </Flex>
 );
 
