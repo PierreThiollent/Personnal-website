2b9047f08fc4cfb7f952fa978e43b898145d99ea
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-08-30 01:21:04 +0200

    👌 REFACTOR: Improve design of footer

--- components/footer.js
+++ components/footer.js
@@ -2,11 +2,11 @@ import { Flex, Image, Link, Text } from '@chakra-ui/core';
 
 const Footer = () => (
   <footer>
-    <Flex justifyContent='center' alignItems='center' py='20px'>
-      <Text color='darkblue'>
+    <Flex justifyContent='center' alignItems='center' py='20px' flexDirection='column'>
+      <Text color='darkblue' fontWeight='300'>
         Developped by Pierre Thiollent with ❤️ and
         <Link href='https://nextjs.org/' isExternal title='Next.js' ml='2px'>
-          <Image src='/favicon.ico' alt='Vercel' maxW='20px' display='inline-block' verticalAlign='text-top' />
+          <Image src='/favicon.ico' alt='Vercel' maxW='21px' display='inline-block' verticalAlign='middle' />
         </Link>
       </Text>
     </Flex>
