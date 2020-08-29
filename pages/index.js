d217599a673bd229580f4331853168b7665f7185
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-08-29 20:04:07 +0200

    📦 FEAT: Add content to the Home page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,96 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Box, Flex, Heading, Image, Link, Text } from '@chakra-ui/core';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Pierre Thiollent - Web Developer</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
+      <style>@import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap');</style>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <Flex justifyContent='center' flexDirection='column' height='80vh' letterSpacing='-.16px'>
+        <Box maxWidth='1230px' px='30px' mx='auto' w='100%'>
+          <Heading as='h1' fontWeight='700' fontSize='28px' color='darkblue' lineHeight='1.2'>
+            Pierre Thiollent
+          </Heading>
+          <Heading as='h2' fontSize='16px' fontWeight='300' color='customGrey' lineHeight='1.625'>
+            💻 Web Developer, UX/UI enthusiast
+          </Heading>
+          <Box mt='25px'>
+            <Link
+              href='https://github.com/PierreThiollent'
+              isExternal
+              fontSize='14px'
+              color='#8B9CAC'
+              fontWeight='300'
+              mr='12px'
+              _hover={{ textDecoration: 'none', color: 'tomato' }}
+              _focus={{ outline: 'none' }}>
+              GitHub
+            </Link>
+            <Link
+              href='https://twitter.com/Pierre_t76'
+              isExternal
+              fontSize='14px'
+              color='#8B9CAC'
+              fontWeight='300'
+              mr='12px'
+              _hover={{ textDecoration: 'none', color: 'tomato' }}
+              _focus={{ outline: 'none' }}>
+              Twitter
+            </Link>
+            <Link
+              href='https://www.linkedin.com/in/pierre-thiollent/'
+              isExternal
+              fontSize='14px'
+              color='#8B9CAC'
+              fontWeight='300'
+              mr='12px'
+              _hover={{ textDecoration: 'none', color: 'tomato' }}
+              _focus={{ outline: 'none' }}>
+              Linkedin
+            </Link>
+            <Link
+              href='https://www.instagram.com/pierre_thiollent/'
+              isExternal
+              fontSize='14px'
+              color='#8B9CAC'
+              fontWeight='300'
+              _hover={{ textDecoration: 'none', color: 'tomato' }}
+              _focus={{ outline: 'none' }}>
+              Instagram
+            </Link>
+          </Box>
+          <Box mt='50px' maxW='700px'>
+            <Text color='customGrey' fontWeight='300'>
+              Hi 👋🏻, I'm Pierre Thiollent, I live in Rouen and I'm a Web and Mobile Developer. I code mostly in Javascript, React and React Native. I
+              practice PHP and Symfony too. Check my work below ! ⬇️
+            </Text>
+          </Box>
+        </Box>
+      </Flex>
+      <Flex justifyContent='center' flexDirection='column'>
+        <Box maxWidth='1230px' px='30px' mx='auto' w='100%'>
+          <Heading as='h3' color='darkblue' fontSize='25px' fontWeight='700'>
+            Portfolio
+          </Heading>
+          <Text color='customGrey' fontWeight='300' lineHeight='1.625'>
+            My latest projects
+          </Text>
+        </Box>
+      </Flex>
+      <footer>
+        <Flex justifyContent='center' alignItems='center' py='20px'>
+          <Text color='darkblue'>
+            Developped by Pierre Thiollent with ❤️ and
+            <Link href='https://nextjs.org/' isExternal title='Next.js' ml='2px'>
+              <Image src='/favicon.ico' alt='Vercel' maxW='20px' display='inline-block' verticalAlign='text-top' />
+            </Link>
+          </Text>
+        </Flex>
       </footer>
-    </div>
-  )
+    </>
+  );
 }
