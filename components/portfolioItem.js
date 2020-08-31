a36872275b8e69a337a10bdc6ed4832b1f496d91
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-08-31 19:45:00 +0200

    📦 FEAT: Add tags

--- components/portfolioItem.js
+++ components/portfolioItem.js
@@ -1,7 +1,7 @@
-import { Box, Flex, Image, Link, Text } from '@chakra-ui/core';
+import { Box, Flex, Image, Link, Stack, Tag, Text } from '@chakra-ui/core';
 
-const PortfolioItem = ({ image, projectName, projectDescription, link }) => (
-  <Flex flex='0 1 370px' mb='30px'>
+const PortfolioItem = ({ image, projectName, projectDescription, link, tags }) => (
+  <Flex flex='0 1 370px' mb='40px' flexDirection='column'>
     <Link _hover={{ textDecoration: 'none' }} href={`${link}`} isExternal>
       <Image src={`/${image}`} borderRadius='5px' maxHeight='370px' />
       <Box mt='20px'>
@@ -13,6 +13,14 @@ const PortfolioItem = ({ image, projectName, projectDescription, link }) => (
         </Text>
       </Box>
     </Link>
+    <Stack mt='10px' spacing={2} isInline>
+      {tags &&
+        tags.map((tag, index) => (
+          <Tag color='#8B9CAC' size='sm' backgroundColor='#f7fafc' fontWeight='300' fontSize='12px' verticalAlign='middle' key={index}>
+            {tag}
+          </Tag>
+        ))}
+    </Stack>
   </Flex>
 );
 
