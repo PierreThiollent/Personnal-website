ab0e6cf7f787c9c9057565fb8a4367d9b882bbee
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-08-31 19:45:44 +0200

    📦 FEAT: Import global style

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ColorModeProvider, CSSReset, ThemeProvider } from '@chakra-ui/core';
 import DarkModeButton from '../components/darkModeButton';
 import Footer from '../components/footer';
+import '../style/global.css';
 import customTheme from '../theme';
 
 function App({ Component, pageProps, children }) {
