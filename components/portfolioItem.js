c95d6b6abcfac56101d456842fce556f85a4f72b
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-09-01 19:30:01 +0200

    📦 FEAT: Display github icon with link if github url is defined

--- components/portfolioItem.js
+++ components/portfolioItem.js
@@ -1,6 +1,6 @@
-import { Box, Flex, Image, Link, Stack, Tag, Text } from '@chakra-ui/core';
+import { Box, Flex, Icon, Image, Link, Stack, Tag, Text } from '@chakra-ui/core';
 
-const PortfolioItem = ({ image, projectName, projectDescription, link, tags }) => (
+const PortfolioItem = ({ image, projectName, projectDescription, link, tags, gitUrl }) => (
   <Flex flex='0 1 370px' mb='40px' flexDirection='column'>
     <Link _hover={{ textDecoration: 'none' }} href={`${link}`} isExternal>
       <Image src={`/${image}`} borderRadius='5px' maxHeight='370px' alt={`${projectName}`} />
@@ -20,6 +20,11 @@ const PortfolioItem = ({ image, projectName, projectDescription, link, tags }) =
             {tag}
           </Tag>
         ))}
+      {gitUrl && (
+        <Link href={`${gitUrl}`} isExternal w='max-content' ml='auto' marginRight='10px'>
+          <Icon name='github' size='15px' color='darkblue' />
+        </Link>
+      )}
     </Stack>
   </Flex>
 );
