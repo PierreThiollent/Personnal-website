39c5eb870d7aaf725f4674cacc830776471d4d4a
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-09-11 14:29:04 +0200

    🧹 CHORE: Add react-lottie-web

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import { Box, Flex, Heading, Link as ChakraLink, Stack, Tag, Text } from '@chakra-ui/core';
+import Lottie from 'lottie-react-web';
 import Head from 'next/head';
 import Link from 'next/link';
 import PortfolioItem from '../components/portfolioItem';
 import { getSortedPostsData } from '../lib/posts';
+import animation from '../public/lottie-animation.json';
 
 export default function Home({ allPostsData }) {
   return (
@@ -13,66 +15,76 @@ export default function Home({ allPostsData }) {
         <meta name='google-site-verification' content='_Tg56-iiZIYfCkGM9nJOIs8j-lrOlCPKL0WmG-FRQVU' />
         <meta name='description' content='Pierre Thiollent, French Web Developer living in Rouen, France.' />
       </Head>
-      <Flex justifyContent='center' flexDirection='column' height='80vh'>
-        <Box maxWidth='1260px' px='30px' mx='auto' w='100%'>
-          <Heading as='h1' fontWeight='700' fontSize='28px' color='darkGrey'>
-            Pierre Thiollent
-          </Heading>
-          <Heading as='h2' fontSize='16px' fontWeight='300' color='customGrey' lineHeight='1.8'>
-            💻 Web Developer, UX/UI enthusiast
-          </Heading>
-          <Box mt='25px'>
-            <ChakraLink
-              href='https://github.com/PierreThiollent'
-              isExternal
-              fontSize='14px'
-              color='lightSlateGrey'
-              fontWeight='300'
-              mr='12px'
-              _hover={{ textDecoration: 'none', color: 'tomato' }}
-              _focus={{ outline: 'none' }}>
-              GitHub
-            </ChakraLink>
-            <ChakraLink
-              href='https://twitter.com/Pierre_t76'
-              isExternal
-              fontSize='14px'
-              color='lightSlateGrey'
-              fontWeight='300'
-              mr='12px'
-              _hover={{ textDecoration: 'none', color: 'tomato' }}
-              _focus={{ outline: 'none' }}>
-              Twitter
-            </ChakraLink>
-            <ChakraLink
-              href='https://www.linkedin.com/in/pierre-thiollent/'
-              isExternal
-              fontSize='14px'
-              color='lightSlateGrey'
-              fontWeight='300'
-              mr='12px'
-              _hover={{ textDecoration: 'none', color: 'tomato' }}
-              _focus={{ outline: 'none' }}>
-              Linkedin
-            </ChakraLink>
-            <ChakraLink
-              href='https://www.instagram.com/pierre_thiollent/'
-              isExternal
-              fontSize='14px'
-              color='lightSlateGrey'
-              fontWeight='300'
-              _hover={{ textDecoration: 'none', color: 'tomato' }}
-              _focus={{ outline: 'none' }}>
-              Instagram
-            </ChakraLink>
+      <Flex alignItems='center' height='80vh'>
+        <Flex maxWidth='1260px' px='30px' mx='auto' w='100%' justifyContent='space-between' alignItems='flex-end'>
+          <Box>
+            <Heading as='h1' fontWeight='700' fontSize='28px' color='darkGrey'>
+              Pierre Thiollent
+            </Heading>
+            <Heading as='h2' fontSize='16px' fontWeight='300' color='customGrey' lineHeight='1.8'>
+              💻 Web Developer, UX/UI enthusiast
+            </Heading>
+            <Box mt='25px'>
+              <ChakraLink
+                href='https://github.com/PierreThiollent'
+                isExternal
+                fontSize='14px'
+                color='lightSlateGrey'
+                fontWeight='300'
+                mr='12px'
+                _hover={{ textDecoration: 'none', color: 'tomato' }}
+                _focus={{ outline: 'none' }}>
+                GitHub
+              </ChakraLink>
+              <ChakraLink
+                href='https://twitter.com/Pierre_t76'
+                isExternal
+                fontSize='14px'
+                color='lightSlateGrey'
+                fontWeight='300'
+                mr='12px'
+                _hover={{ textDecoration: 'none', color: 'tomato' }}
+                _focus={{ outline: 'none' }}>
+                Twitter
+              </ChakraLink>
+              <ChakraLink
+                href='https://www.linkedin.com/in/pierre-thiollent/'
+                isExternal
+                fontSize='14px'
+                color='lightSlateGrey'
+                fontWeight='300'
+                mr='12px'
+                _hover={{ textDecoration: 'none', color: 'tomato' }}
+                _focus={{ outline: 'none' }}>
+                Linkedin
+              </ChakraLink>
+              <ChakraLink
+                href='https://www.instagram.com/pierre_thiollent/'
+                isExternal
+                fontSize='14px'
+                color='lightSlateGrey'
+                fontWeight='300'
+                _hover={{ textDecoration: 'none', color: 'tomato' }}
+                _focus={{ outline: 'none' }}>
+                Instagram
+              </ChakraLink>
+            </Box>
+            <Box mt='50px' maxW='700px'>
+              <Text color='customGrey' fontWeight='300'>
+                Hi 👋🏻, I'm Pierre Thiollent, I live in Rouen and I'm a Web and Mobile Developer. I code mostly in Javascript, React and React Native.
+                I practice PHP and Symfony too. Check my work below ! ⬇️
+              </Text>
+            </Box>
           </Box>
-          <Box mt='50px' maxW='700px'>
-            <Text color='customGrey' fontWeight='300'>
-              Hi 👋🏻, I'm Pierre Thiollent, I live in Rouen and I'm a Web and Mobile Developer. I code mostly in Javascript, React and React Native. I
-              practice PHP and Symfony too. Check my work below ! ⬇️
-            </Text>
-          </Box>
-        </Box>
+          <Lottie
+            options={{
+              animationData: animation,
+            }}
+            width={150}
+            height={150}
+            style={{ margin: 0 }}
+          />
+        </Flex>
       </Flex>
       <Box>
         <Box maxWidth='1260px' px='30px' mx='auto' w='100%'>
