86ea04ce49ede3cd228b5b2db36e5e566e419744
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-10-09 14:33:32 +0200

    📦 FEAT: Add color mode for post detail page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,27 +1,29 @@
-import { Box } from '@chakra-ui/core';
+import { Box, useColorMode } from '@chakra-ui/core';
 import Head from 'next/head';
 import Link from 'next/link';
 import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export default function Post({ postData }) {
-  console.log(postData);
+  const { colorMode } = useColorMode();
   return (
     <>
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <Box maxWidth='1260px' px='30px' mx='auto' w='100%'>
-        <article>
-          <h1>{postData.title}</h1>
-          <div>
-            <Date dateString={postData.date} />
-          </div>
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </article>
-        <Link href='/'>
-          <a>← Back to home</a>
-        </Link>
+      <Box backgroundColor={`${colorMode}.background`}>
+        <Box maxWidth='1260px' px='30px' mx='auto' w='100%'>
+          <article>
+            <h1>{postData.title}</h1>
+            <div>
+              <Date dateString={postData.date} />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+          </article>
+          <Link href='/'>
+            <a>← Back to home</a>
+          </Link>
+        </Box>
       </Box>
     </>
   );
